bbf86abae17e0c048c7938cf0a64f5b1c916c608
ewinter2 <elliewinter3@gmail.com>
2026-04-11 13:29:07 -0700

    changes for setting up apple dev program

--- RallyAI/AppConfig.swift
+++ RallyAI/AppConfig.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 enum AppConfig {
-    #if DEBUG
+    #if DEBUG && targetEnvironment(simulator)
     static let backendBaseURL: URL = URL(string: "http://127.0.0.1:8000")!
     #else
     static let backendBaseURL: URL = URL(string: "https://web-production-98ea9.up.railway.app")!
